55323dd28d5884d5cdb81d5bc23820e68d7bc04c
Cristian-Mihai Rosiu <rosiucristian10@gmail.com>
2020-04-30 17:57:11 +0000

    -Finished FOV -Added mesh renderer for FOV

--- Assets/Scripts/FOVEditor.cs
+++ Assets/Scripts/FOVEditor.cs
@@ -12,10 +12,16 @@ public class FOVEditor : Editor
         FieldOfView fow = (FieldOfView)target;
         Handles.color = Color.white;
         Handles.DrawWireArc(fow.transform.position, Vector3.forward, Vector3.right, 360, fow.viewRadius);
-        Vector2 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
-        Vector2 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+        Vector2 viewAngleA = fow.DirFromAngle(fow.viewAngle / 2, false);
+        Vector2 viewAngleB = fow.DirFromAngle(-fow.viewAngle / 2, false);
 
         Handles.DrawLine(fow.transform.position, (Vector2)fow.transform.position + viewAngleA * fow.viewRadius);
         Handles.DrawLine(fow.transform.position, (Vector2)fow.transform.position + viewAngleB * fow.viewRadius);
+
+        Handles.color = Color.red;
+        foreach(Transform visibleTarget in fow.visibleTargets)
+        {
+            Handles.DrawLine(fow.transform.position, visibleTarget.position);
+        }
     }
 }
