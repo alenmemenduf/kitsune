2e187d6d7459b65e970fa30d143a698f3bd610aa
Cristian-Mihai Rosiu <rosiucristian10@gmail.com>
2020-04-30 15:15:40 +0000

    - Added Camera Enemy - Added FOW to the camera enemy

---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float viewRadius;
+    [Range(0,360)]
+    public float viewAngle;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    public List<Transform> visibleTargets = new List<Transform>();
+    public Vector2 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees -= transform.eulerAngles.z;
+        }
+        return new Vector2(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+    IEnumerator FindTargetWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+    public void FindVisibleTargets()
+    {
+        Collider2D[] targetsInViewRadius = Physics2D.OverlapCircleAll(transform.position, viewRadius, targetMask);
+        for(int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector2 dirToTarget = (target.position - transform.position).normalized;
+            if(Vector3.Angle (transform.right, dirToTarget) < viewAngle / 2)
+            {
+                float distToTarget = Vector2.Distance(transform.position, target.position);
+
+                if(!Physics2D.Raycast(transform.position, dirToTarget,distToTarget, obstacleMask))
+                {
+                    visibleTargets.Add(target);
+                }
+            }
+        }
+    }
+}
